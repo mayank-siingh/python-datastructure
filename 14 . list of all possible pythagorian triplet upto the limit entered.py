7d97d3be42017a16d83ea10d4e52f6186e91d436
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2019-08-17 09:39:37 +0530

    Update 14 . list of all possible pythagorian triplet upto the limit entered.py

--- 14 . list of all possible pythagorian triplet upto the limit entered.py
+++ 14 . list of all possible pythagorian triplet upto the limit entered.py
@@ -2,11 +2,12 @@ n = int(input("Enter the number upto which you want all possible pair of pythago
 def pythagorus(x):
 	list = []
 	for i in range(1,x):
-		for j in range(1,x):
-			for k in range(1,x):
-				if k**2 + j**2 == i**2:
+		for j in range(i,x):
+			for k in range(j,x):
+				if i**2 + j**2 == k**2:
 					list.append([k,j,i])
 	print(len(list))
 	return(list)
 call = pythagorus(n)
 print(call)
+
